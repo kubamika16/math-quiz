8814000cad6d32e9849049855048d81318dbea23
kubamika16 <kuba.mika16@gmail.com>
2022-05-13 10:34:04 +0200

    first commit

---
+++ .ask/lambda/index.js
@@ -0,0 +1,365 @@
+//Problemy na które jeszcze nie znalazłem rozwiązania:
+//Gdy Alexa rozpocznie program, a uzytkownik powie 'dificult', zamiast 'hard', Alexa przejdzie od razu do odpowiedzi na pytanie matematyczne. Dodać do Handlera ResultIntentHandler funkcję if(level===undefined)...
+
+const Alexa = require("ask-sdk-core");
+const functions = require("./helpers/functions");
+const equationsEasy = require("./helpers/equationsEasy");
+const equationsMedium = require("./helpers/equationsMedium");
+const equationsHard = require("./helpers/equationsHard");
+const equationsExtreme = require("./helpers/equationsExtreme");
+
+let level;
+let points = 0;
+let count = 4;
+let allQuestions;
+let currentQuestion;
+let additionalTime;
+let speakOutput = "";
+let repromptText = "";
+
+const reset = function () {
+  points = 0;
+  count = 4;
+};
+
+const LaunchRequestHandler = {
+  canHandle(handlerInput) {
+    return (
+      Alexa.getRequestType(handlerInput.requestEnvelope) === "LaunchRequest"
+    );
+  },
+  handle(handlerInput) {
+    // const speakOutput = 'Welcome, you can say Hello or Help. Which would you like to try?';
+    speakOutput = functions.randomFromArray(functions.messages.welcome);
+    functions.newEquations();
+    level = undefined;
+
+    return handlerInput.responseBuilder
+      .speak(speakOutput)
+      .reprompt(speakOutput)
+      .getResponse();
+  },
+};
+const GameLevelIntentHandler = {
+  canHandle(handlerInput) {
+    return (
+      Alexa.getRequestType(handlerInput.requestEnvelope) === "IntentRequest" &&
+      Alexa.getIntentName(handlerInput.requestEnvelope) === "GameLevelIntent"
+    );
+  },
+  handle(handlerInput) {
+    speakOutput = "";
+    level = handlerInput.requestEnvelope.request.intent.slots.level.value;
+    speakOutput += `${functions.randomFromArray(
+      functions.messages.choosenLevel
+    )} ${level}. `;
+
+    if (level === "medium") {
+      speakOutput += `Because of that level, you will have extra 3 seconds. Alright! `;
+    }
+    if (level === "hard") {
+      speakOutput += `Because of that level, you will have extra 6 seconds. Alright! `;
+    }
+    if (level === "extreme") {
+      speakOutput += `Because of that level, you will have extra 10 seconds. Alright! `;
+    }
+
+    reset();
+    functions.newEquations();
+
+    if (level === "easy") {
+      allQuestions = equationsEasy.equations;
+      currentQuestion = allQuestions[count];
+      speakOutput += currentQuestion.questionInWords;
+
+      additionalTime = `${functions.audio.additionalTime(0)}`;
+    } else if (level === "medium") {
+      allQuestions = equationsMedium.createQuestions();
+      console.log(allQuestions);
+      currentQuestion = allQuestions[count];
+      speakOutput += currentQuestion.questionInWords;
+
+      additionalTime = `${functions.audio.additionalTime(3)} ${
+        functions.audio.answerTime
+      }`;
+      speakOutput += ` ${additionalTime}`;
+    } else if (level === "hard") {
+      allQuestions = equationsHard.equations;
+      currentQuestion = allQuestions[count];
+      speakOutput += currentQuestion.questionInWords;
+
+      additionalTime = `${functions.audio.additionalTime(6)} ${
+        functions.audio.answerTime
+      }`;
+      speakOutput += ` ${additionalTime}`;
+    } else if (level === "extreme") {
+      allQuestions = equationsExtreme.equations;
+      currentQuestion = allQuestions[count];
+      speakOutput += currentQuestion.questionInWords;
+
+      additionalTime = `${functions.audio.additionalTime(10)} ${
+        functions.audio.answerTime
+      }`;
+      speakOutput += ` ${additionalTime}`;
+    } else {
+      speakOutput = functions.randomFromArray(
+        functions.messages.levelNotUnderstood
+      );
+    }
+    console.log("Level:", level);
+
+    return handlerInput.responseBuilder
+      .speak(speakOutput)
+      .reprompt(functions.audio.repromptClock)
+      .getResponse();
+  },
+};
+
+const ResultIntentHandler = {
+  canHandle(handlerInput) {
+    return (
+      Alexa.getRequestType(handlerInput.requestEnvelope) === "IntentRequest" &&
+      Alexa.getIntentName(handlerInput.requestEnvelope) === "ResultIntent"
+    );
+  },
+  handle(handlerInput) {
+    console.log("Level:", level);
+    //Jeśli poziom nie został wybrany, zwrócona zostanie od razu odpowiedź zeby wybrac level
+    if (level === undefined) {
+      speakOutput = "Choose the level first: easy, medium, hard or extreme.";
+
+      return handlerInput.responseBuilder
+        .speak(speakOutput)
+        .reprompt("What is the level you want to choose?")
+        .getResponse();
+    }
+
+    speakOutput = "";
+    repromptText = "";
+
+    const userResult =
+      handlerInput.requestEnvelope.request.intent.slots.result.value;
+
+    if (Number(userResult) === currentQuestion.result) {
+      speakOutput += ` ${functions.audio.correctAnswer} Correct! That will be ${currentQuestion.result}. `;
+      console.log(currentQuestion);
+      console.log("Count:", count);
+      points++;
+      count--;
+      if (count >= 0) {
+        currentQuestion = allQuestions[count];
+        speakOutput += `${functions.randomFromArray(
+          functions.messages.nextQuestion
+        )} ${currentQuestion.questionInWords}? `;
+        speakOutput += ` ${additionalTime} `;
+        repromptText = functions.audio.repromptClock;
+      } else {
+        speakOutput += `Alright! You correctly answered ${points} out of 5 questions, earning ${functions.addingPoints(
+          level,
+          points
+        )} points. `;
+        repromptText = "";
+      }
+    } else {
+      speakOutput = `${
+        functions.audio.incorrectAnswer
+      } Unfortunately, ${functions.randomFromArray(
+        functions.messages.result
+      )} ${currentQuestion.result}. `;
+      count--;
+      if (count >= 0) {
+        currentQuestion = allQuestions[count];
+        speakOutput += `${functions.randomFromArray(
+          functions.messages.nextQuestion
+        )} ${currentQuestion.questionInWords}?`;
+        speakOutput += ` ${additionalTime} `;
+        repromptText = functions.audio.repromptClock;
+      } else {
+        speakOutput += `Alright! You correctly answered ${points} out of 5 questions, earning ${functions.addingPoints(
+          level,
+          points
+        )} points. `;
+        repromptText = "";
+      }
+    }
+
+    return handlerInput.responseBuilder
+      .speak(speakOutput)
+      .reprompt(repromptText)
+      .getResponse();
+  },
+};
+
+const dontKnowIntentHandler = {
+  canHandle(handlerInput) {
+    return (
+      Alexa.getRequestType(handlerInput.requestEnvelope) === "IntentRequest" &&
+      Alexa.getIntentName(handlerInput.requestEnvelope) === "dontKnowIntent"
+    );
+  },
+  handle(handlerInput) {
+    //Jeśli poziom nie został wybrany, zwrócona zostanie od razu odpowiedź zeby wybrac level
+    if (level === undefined) {
+      speakOutput = "Choose the level first: easy, medium or hard.";
+
+      return handlerInput.responseBuilder
+        .speak(speakOutput)
+        .reprompt("What is the level you want to choose?")
+        .getResponse();
+    }
+
+    speakOutput = "";
+    repromptText = "";
+
+    speakOutput += `No worries. ${functions.randomFromArray(
+      functions.messages.result
+    )} ${currentQuestion.result}. `;
+    count--;
+    if (count >= 0) {
+      currentQuestion = allQuestions[count];
+      speakOutput += `${functions.randomFromArray(
+        functions.messages.nextQuestion
+      )} ${currentQuestion.questionInWords}?`;
+      speakOutput += ` ${additionalTime} `;
+      repromptText = functions.audio.repromptClock;
+    } else {
+      speakOutput += `Alright! You correctly answered ${points} out of 5 questions, earning ${functions.addingPoints(
+        level,
+        points
+      )} points. `;
+      repromptText = "";
+    }
+
+    return handlerInput.responseBuilder
+      .speak(speakOutput)
+      .reprompt(repromptText)
+      .getResponse();
+  },
+};
+
+const RepatQuestionIntentHandler = {
+  canHandle(handlerInput) {
+    return (
+      Alexa.getRequestType(handlerInput.requestEnvelope) === "IntentRequest" &&
+      Alexa.getIntentName(handlerInput.requestEnvelope) ===
+        "RepatQuestionIntent"
+    );
+  },
+  handle(handlerInput) {
+    let speakOutputRepeat = `Of course! ${speakOutput}`;
+
+    return handlerInput.responseBuilder
+      .speak(speakOutputRepeat)
+      .reprompt(`${repromptText} k`)
+      .getResponse();
+  },
+};
+const HelpIntentHandler = {
+  canHandle(handlerInput) {
+    return (
+      Alexa.getRequestType(handlerInput.requestEnvelope) === "IntentRequest" &&
+      Alexa.getIntentName(handlerInput.requestEnvelope) === "AMAZON.HelpIntent"
+    );
+  },
+  handle(handlerInput) {
+    speakOutput = functions.randomFromArray(functions.messages.help);
+
+    return handlerInput.responseBuilder
+      .speak(speakOutput)
+      .reprompt(
+        "So if you want, choose a level to start playing: easy, medium, hard, or extreme."
+      )
+      .getResponse();
+  },
+};
+
+const CancelAndStopIntentHandler = {
+  canHandle(handlerInput) {
+    return (
+      Alexa.getRequestType(handlerInput.requestEnvelope) === "IntentRequest" &&
+      (Alexa.getIntentName(handlerInput.requestEnvelope) ===
+        "AMAZON.CancelIntent" ||
+        Alexa.getIntentName(handlerInput.requestEnvelope) ===
+          "AMAZON.StopIntent")
+    );
+  },
+  handle(handlerInput) {
+    const speakOutput =
+      "Hope you had fun! If you can, leave a review. Good or bad, it should be honest. Honest answers will make this game much better. Goodbye!";
+    return handlerInput.responseBuilder.speak(speakOutput).getResponse();
+  },
+};
+const SessionEndedRequestHandler = {
+  canHandle(handlerInput) {
+    return (
+      Alexa.getRequestType(handlerInput.requestEnvelope) ===
+      "SessionEndedRequest"
+    );
+  },
+  handle(handlerInput) {
+    // Any cleanup logic goes here.
+    reset();
+    level = undefined;
+    // functions.newEquations();
+    return handlerInput.responseBuilder.getResponse();
+  },
+};
+
+// The intent reflector is used for interaction model testing and debugging.
+// It will simply repeat the intent the user said. You can create custom handlers
+// for your intents by defining them above, then also adding them to the request
+// handler chain below.
+const IntentReflectorHandler = {
+  canHandle(handlerInput) {
+    return (
+      Alexa.getRequestType(handlerInput.requestEnvelope) === "IntentRequest"
+    );
+  },
+  handle(handlerInput) {
+    const intentName = Alexa.getIntentName(handlerInput.requestEnvelope);
+    const speakOutput = `You just triggered ${intentName}`;
+
+    return (
+      handlerInput.responseBuilder
+        .speak(speakOutput)
+        //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
+        .getResponse()
+    );
+  },
+};
+
+// Generic error handling to capture any syntax or routing errors. If you receive an error
+// stating the request handler chain is not found, you have not implemented a handler for
+// the intent being invoked or included it in the skill builder below.
+const ErrorHandler = {
+  canHandle() {
+    return true;
+  },
+  handle(handlerInput, error) {
+    console.log(`~~~~ Error handled: ${error.stack}`);
+    const speakOutput = `Sorry, I had trouble doing what you asked. Please try again.`;
+
+    return handlerInput.responseBuilder
+      .speak(speakOutput)
+      .reprompt(speakOutput)
+      .getResponse();
+  },
+};
+
+// The SkillBuilder acts as the entry point for your skill, routing all request and response
+// payloads to the handlers above. Make sure any new handlers or interceptors you've
+// defined are included below. The order matters - they're processed top to bottom.
+exports.handler = Alexa.SkillBuilders.custom()
+  .addRequestHandlers(
+    LaunchRequestHandler,
+    GameLevelIntentHandler,
+    ResultIntentHandler,
+    dontKnowIntentHandler,
+    HelpIntentHandler,
+    RepatQuestionIntentHandler,
+    CancelAndStopIntentHandler,
+    SessionEndedRequestHandler,
+    IntentReflectorHandler // make sure IntentReflectorHandler is last so it doesn't override your custom intent handlers
+  )
+  .addErrorHandlers(ErrorHandler)
+  .lambda();
---
+++ lambda/helpers/functions.js
@@ -0,0 +1,96 @@
+const equationsEasy = require("./equationsEasy");
+const equationsMedium = require("./equationsMedium");
+const equationsHard = require("./equationsHard");
+const equationsExtreme = require("./equationsExtreme");
+
+const randomFromArray = (array) =>
+  array[Math.floor(Math.random() * array.length)];
+
+const messages = {
+  welcome: [
+    "Hi! Welcome in the math quiz! To begin, say the level you want to start: easy, medium, hard, or extreme",
+    "Hello! This is a math quiz game. I will ask you five questions, and you have some time to give me correct answer. Now, choose your level: easy, medium, hard, or extreme?",
+    "Holla! Happy to see you! This is a math quiz game. To start, pick a level: easy, medium, hard, or extreme?",
+    "How are you? If you want to play this math quiz game, pick a level: easy, medium, hard, or extreme?",
+    "Dzien Dobry! You opened a math quiz app. To start, choose a level: easy, medium, hard, or extreme",
+    // "Hello! To start the game, pick the level: easy, medium, hard, extreme."
+  ],
+  levelNotUnderstood: [
+    "Sorry, could you say again which level you would like to choose?",
+    "I'm afraid I didn't hear that. Could you say that lvel again?",
+    "Hmm, I'm not sure. Level easy, medium, hard, or extreme?",
+    "Sorry, I have small problem with hearing. Could you tell me a level once again?",
+    "I am really sorry, but I didn't get that. Say the level again.",
+    "I am still young assistant, and still learning to listen. Could you tell me once again what level would you like to choose?",
+  ],
+  choosenLevel: [
+    "Alright! Let's begin with level",
+    "Awesome, level",
+    "Ok, I will give you questions for level",
+    "Good choice! Level",
+    "Ok! Good luck with level",
+    "You have choosen level",
+  ],
+  nextQuestion: [
+    "Next question:",
+    "Next:",
+    "Another one:",
+    "Moving on:",
+    "Upcoming question:",
+    "Ok:",
+    "This one is tricky:",
+    "Be careful, next one:",
+  ],
+  result: [
+    "That will be",
+    "Result:",
+    "The result is:",
+    "Answer for that question is",
+    "Solution for that question is",
+    "It is",
+  ],
+  help: [
+    ` This is a math quiz game. I will ask you five questions, and you will have to tell the correct answer. This game has three levels. Easy, medium and hard. For every easy question, you get one point. Medium, 3 points, and hard, five points. To begin, say the level you want to start.`,
+  ],
+};
+
+// const additionalTime = (seconds) => `<break time="${seconds}s"/>`;
+
+const audio = {
+  repromptClock: `<audio src="soundbank://soundlibrary/alarms/beeps_and_bloops/clock_02"/>`,
+  correctAnswer: `<audio src="soundbank://soundlibrary/alarms/beeps_and_bloops/bell_02"/>`,
+  incorrectAnswer: `<audio src="soundbank://soundlibrary/alarms/beeps_and_bloops/buzz_04"/>`,
+  answerTime: `<audio src="soundbank://soundlibrary/alarms/beeps_and_bloops/bell_04"/>`,
+
+  additionalTime(seconds) {
+    return `<break time="${seconds}s"/>`;
+  },
+};
+
+const addingPoints = (level, correct) => {
+  if (level === "easy") return correct;
+  if (level === "medium") return correct * 3;
+  if (level === "hard") return correct * 5;
+  if (level === "extreme") return correct * 10;
+};
+
+const newEquations = function () {
+  equationsEasy.equations.length = 0;
+  equationsEasy.createQuestions();
+
+  equationsMedium.equations.length = 0;
+
+  equationsHard.equations.length = 0;
+  equationsHard.createQuestions();
+
+  equationsExtreme.equations.length = 0;
+  equationsExtreme.createQuestions();
+};
+
+module.exports = {
+  messages,
+  randomFromArray,
+  newEquations,
+  addingPoints,
+  audio,
+};
